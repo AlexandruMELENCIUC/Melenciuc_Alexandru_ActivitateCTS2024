aab092aafe2c63c6aff0fabea8c1f4687b215c61
Melenciuc Claudiu Alexandru <melenciucclaudiu20@stud.ase.ro>
2024-05-18 22:27:03 +0300

    v1

---
+++ E11_Sportiv/src/IVerifica.java
@@ -0,0 +1,3 @@
+public interface IVerifica {
+    void verificare();
+}
---
+++ E11_Sportiv/src/VerificaVIP.java
@@ -0,0 +1,6 @@
+public class VerificaVIP implements IVerifica {
+    @Override
+    public void verificare() {
+        System.out.println("Se verifica biletul spectatorului VIP!");
+    }
+}
