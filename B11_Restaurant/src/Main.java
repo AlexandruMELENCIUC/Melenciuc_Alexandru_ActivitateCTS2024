ad5d5d3fdfc9db78150dad2bcd786f4ba86e7fbe
Melenciuc Claudiu Alexandru <melenciucclaudiu20@stud.ase.ro>
2024-05-23 18:18:39 +0300

    v1

--- B11_Restaurant/src/Main.java
+++ B11_Restaurant/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        ModPlata modPlata = new ModPlata();
 
+        modPlata.setStrategiePlata(new PlataCard());
+        modPlata.efectueazaPlata(1350.0f);
+
+        modPlata.setStrategiePlata(new PlataCash());
+        modPlata.efectueazaPlata(457.5f);
     }
 }
